85e1e9ad49a7e217a222e9027e384e37dbf9d640
TranTheBach <thebach25@gmail.com>
2023-01-29 10:30:09 +0700

    Initial commit

---
+++ PenisGun/Cum.cs
@@ -0,0 +1,43 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace PenisGun
+{
+    public class CumShot : Bullet
+    {
+        public ModExtension_CumShot Extenstion => def.GetModExtension<ModExtension_CumShot>();
+
+        HediffDef hediffType;
+
+        protected override void Impact(Thing hitThing, bool blockedByShield = false)
+        {
+            hediffType = Extenstion.hediffToAdd;
+
+            base.Impact(hitThing, blockedByShield);
+
+            if (Extenstion != null && hitThing != null && hitThing is Pawn pawn)
+            {
+                float rand = Rand.Value;
+                if (rand <= Extenstion.addHediffChance)
+                {
+                    Hediff pawnAvailable = pawn.health?.hediffSet?.GetFirstHediffOfDef(Extenstion.hediffToAdd);
+
+
+                    float randomSeverity = Rand.Range(0.15f, 0.30f);
+                    if (pawnAvailable == null)
+                    {
+                        Hediff hediff = HediffMaker.MakeHediff(hediffType, pawn);
+                        hediff.Severity = randomSeverity;
+                        pawn.health.AddHediff(hediff);
+                    }
+                    else return;
+                }
+            }
+        }
+    }
+}
---
+++ PenisGun/PenisGun.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace PenisGun
+{
+    public class ModExtension_CumShot : DefModExtension
+    {
+        public float addHediffChance = 0.5f;
+        public HediffDef hediffToAdd;
+    }
+
+    //public class ModExtension_PenisGun : DefModExtension
+    //{
+        
+    //}
+}
